9c217a4b92ac64a2f75ce61125c919f62399bb56
OlenaBurduzha <102984984+OlenaBurduzha@users.noreply.github.com>
2022-10-31 08:14:19 +0000

    Deploying to gh-pages from @ OlenaBurduzha/goit-js-hw-09@601c671c9e0c8892367c64fdc8a91bbec248518d 🚀

--- 01-color-switcher.31b9ac59.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),e.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.31b9ac59.js.map
